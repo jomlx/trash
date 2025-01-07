d8ff10557870003c4b3d3e8f1909a78a32a37d78
jomlx <mislos.jomel@gmail.com>
2025-01-07 15:55:46 +0800

    add hover for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,14 @@ function Navbar() {
     return (
         <nav className="w-[400px] shadow-md flex justify-center items-center py-4">
         <div className="inline-flex w-full justify-evenly">
-            <Link to="/"> <span>Library</span></Link>
-            <Link to='/reviewer'> <span>Reviewer</span></Link>
+            <Link to="/" className="relative text-gray-800 hover:text-blue-500 after:absolute after:left-0 after:bottom-0
+            hover:after:scale-x-100 after:h-[3px] after:w-0 after:bg-blue-500 after:transition-all after:duration-300 hover:after:w-full">
+            Library
+            </Link>
+            <Link to='/reviewer' className="relative text-gray-800 hover:text-blue-500 after:absolute after:left-0 after:bottom-0
+            hover:after:scale-x-100 after:h-[3px] after:w-0 after:bg-blue-500 after:transition-all after:duration-300 hover:after:w-full">
+                Reviewer
+            </Link>
             <span>Notes</span>
             <span>Settings</span>
         </div>
